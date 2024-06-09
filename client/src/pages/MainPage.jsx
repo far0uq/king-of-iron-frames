2bd2aa198d995df39248c2309cad51a0e86a1844
far0uq <farooqhaider561@gmail.com>
2024-06-09 15:00:37 +0500

    ELEMENT ALIGNMENT - MainPage.jsx: Implemented all the main elements of the page. Used bootstrap for alignment. - MainPage.css: Style for the component. - assets: Added the input images.
    Upcoming: Fix the width issues in Mainpage.jsx

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,16 +1,45 @@
 import "./MainPage.css";
+import direction from "../assets/direction.png";
+import neutral from "../assets/neutral.png";
+import two from "../assets/two.png";
 
 export default function MainPage() {
   return (
-    <div>
-      <header className="iron-frames-header">
-        <h1>
+    <div className="container">
+      <header className="iron-frames-header row justify-content-center">
+        <h1 className="col-7">
           King of <br /> Iron Frames
         </h1>
-        <h3>Practice your TEKKEN Just-Frame Inputs!</h3>
+        <h3 className="col-7">Practice your TEKKEN Just-Frame Inputs!</h3>
       </header>
 
-      <section className="iron-frames-body">
+      <section className="inputs d-flex row justify-content-between col-8 mx-auto mt-5">
+        <div>
+          <img src={direction} className="input-block" />
+        </div>
+        <div>
+          <img src={neutral} className="input-block" />
+        </div>
+        <div>
+          <img
+            src={direction}
+            style={{ transform: "rotate(90deg)" }}
+            className="input-block"
+          />
+        </div>
+        <div>
+          <img
+            src={direction}
+            style={{ transform: "rotate(45deg)" }}
+            className="input-block"
+          />
+        </div>
+        <div>
+          <img src={two} className="input-block" />
+        </div>
+      </section>
+
+      <section className="iron-frames-settings row justify-content-between mx-auto col-8 mt-5">
         <select>
           <option value="EWGF">Devil Jin ~ EWGF</option>
         </select>
@@ -18,7 +47,7 @@ export default function MainPage() {
         <button>Controls</button>
       </section>
 
-      <footer>Made by Farouq with ⚡</footer>
+      <footer className="iron-frames-footer">Made by Farouq with ⚡</footer>
     </div>
   );
 }
