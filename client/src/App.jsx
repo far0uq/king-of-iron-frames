298c982536776c8e869c2fdd2f962c15e3bbac67
far0uq <farooqhaider561@gmail.com>
2024-06-09 15:39:47 +0500

    CURSOR, FAVICON AND NAME - Added Custom favicon, Project Name and added curson animation library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
 import MainPage from "./pages/MainPage.jsx";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div>
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={10}
+        color="0,0,0"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={3}
+        outerStyle={{
+          border: "2px solid black",
+        }}
+      />
       <MainPage />
     </div>
   );
