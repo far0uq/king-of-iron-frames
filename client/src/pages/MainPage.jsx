e8675d97abd70083e90790ff292e0be2e4277ea6
far0uq <ekkarona@gmail.com>
2024-07-07 18:48:23 +0500

    BASE KEYBOARD EVENT LOGIC CREATED - Proper eventListeners in place, reading input correctly. - Base sequence capturing implemented.

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,8 +2,84 @@ import "./MainPage.css";
 import direction from "../assets/direction.png";
 import neutral from "../assets/neutral.png";
 import two from "../assets/two.png";
+import { useState, useEffect } from "react";
+
+let inputNumber = 0;
+let keysPressed = {};
+const ewgf = [
+  {
+    input: "ArrowRight",
+    delay: -1,
+    type: 3,
+  },
+  {
+    input: "ArrowDown",
+    delay: 500,
+    type: 1,
+  },
+  {
+    input: "ArrowDown",
+    delay: 200,
+    type: 1,
+  },
+  {
+    input: "ArrowRight",
+    delay: 0,
+    type: 1,
+  },
+  {
+    input: "2",
+    delay: 0,
+    type: 2,
+  },
+];
 
 export default function MainPage() {
+  // Conditions for Iron Frames
+  const [move, setMove] = useState(ewgf);
+
+  const handleKeyDown = (e) => {
+    keysPressed[e.key] = true;
+
+    console.log("Key Pressed: ", e.key);
+    if (move[inputNumber].input === e.key) {
+      if (move[inputNumber].type === 2) {
+        if (
+          inputNumber > 0 &&
+          keysPressed[move[inputNumber - 1].input] &&
+          keysPressed[move[inputNumber].input]
+        ) {
+          console.log("Correct Input!");
+          inputNumber = inputNumber + 1;
+        }
+      } else {
+        console.log("Correct Input!");
+        inputNumber = inputNumber + 1;
+      }
+
+      if (inputNumber === ewgf.length) {
+        console.log("You did it!");
+        inputNumber = 0;
+      }
+    } else {
+      inputNumber = 0;
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    delete keysPressed[e.key];
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
     <div className="main-box d-flex align-items-center">
       <div className="container">
